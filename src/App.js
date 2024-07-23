5e58951ce300cfa7a4e629b62b877f182a5083c7
Mkhalid000 <khalid@affyclouditsolutions.com>
2024-07-23 11:58:17 +0530

    Adding UI updates

--- src/App.js
+++ src/App.js
@@ -17,10 +17,6 @@ import { Login } from "./Pages/Login";
 
 function App() {
 
-  // useEffect(() => {
-  //   localStorage.setItem("cfs_token", `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Njk4OTU2ODI3Mzc4NzMzOGZiNzgzZTMiLCJmaXJzdE5hbWUiOiJBbWJyZWVuIiwibGFzdE5hbWUiOiJTeWVkIiwiZW1haWwiOiJhbWJyZWVuMkBnbWFpbC5jb20iLCJnZW5kZXIiOiJGZW1hbGUiLCJjaXR5IjoiQmhvcGFsIiwic3RhdGUiOiJNYWRoeWEgUHJhZGVzaCIsIm9jY3VwYXRpb24iOiJTb2Z0d2FyZSBEZXZlbG9wZXIiLCJzdGF0dXMiOnRydWUsImNyZWF0ZWRBdCI6IjIwMjQtMDctMThUMDQ6MDk6MTIuMDY4WiIsInVwZGF0ZWRBdCI6IjIwMjQtMDctMThUMDQ6MDk6MTIuMDc3WiIsIl9fdiI6MCwicHJvZmlsZVBpY3R1cmUiOiJodHRwOi8vbG9jYWxob3N0Ojg3MTUvcHVibGljL2RhdGEvcHJvZmlsZS82Njk4OTU2ODI3Mzc4NzMzOGZiNzgzZTMvMjAyNDA3MThUMDQwOTEyMDcxWjI0MzA5NS5qcGciLCJpYXQiOjE3MjE0OTQ1MDIsImV4cCI6MTcyMTU4MDkwMn0.nWHg8k1JYZoskovolX1PED5MLKV7n0nLuoCFr2F3Iyg`)
-  // }, [])
-
   return (
     <div>
       <Toaster position="top-right" />
